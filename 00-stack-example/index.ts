57f1b99f53ccb757dd3d3cc5a4ab856a59f782b6
Elisabeth Lichtie <elisabethlichtie99@gmail.com>
2026-04-01 16:36:22 -0500

    use config

--- 00-stack-example/index.ts
+++ 00-stack-example/index.ts
@@ -2,16 +2,28 @@ import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 
+const config = new pulumi.Config();
+
+const name = config.require("name");
+const region = config.require("region");
+const environment = config.require("environment");
+
+const tags = { region, environment };
+
 // Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
+const resourceGroup = new resources.ResourceGroup(`${name}-rg`, {
+  location: region,
+  tags,
+});
 
 // Create an Azure Storage Account
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
+const storageAccount = new storage.StorageAccount(`${name}-sa`, {
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    name: storage.SkuName.Standard_LRS,
+  },
+  kind: storage.Kind.StorageV2,
+  tags,
 });
 
 // Export the storage account name
